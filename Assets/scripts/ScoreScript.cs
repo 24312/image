f2e6e78daa60cdf015636c459d9193b059e21109
24312 <35424990+24312@users.noreply.github.com>
2018-04-22 14:07:18 +0200

    Add files via upload
    blyat

---
+++ Assets/scripts/ScoreScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+
+    public int score = 5;
+    private Text textComponent;
+	void Start ()
+    {
+        textComponent = GetComponent<Text>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textComponent.text = "Score: " + score;
+	}
+     public void Addscore()
+    {
+        score += 1;
+    }
+}
---
+++ Assets/scripts/ScoreScriptMin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScriptMin : MonoBehaviour
+{
+
+    public int score = 5;
+    private Text textComponent;
+    void Start ()
+    {
+        textComponent = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        textComponent.text = "Score: " + score;
+    }
+    public void Removescore()
+    {
+        score -= 1;
+    }
+}
